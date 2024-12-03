559d1d383f9eba10e3b6c74a050acecf8faa4aa3
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-12-04 05:46:43 +0800

    added /stats for all models (only api)

--- music_groups/api.py
+++ music_groups/api.py
@@ -2,7 +2,7 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Count
+from django.db.models import Count, Min, Max
 
 from music_groups.models import *
 from music_groups.serializers import *
@@ -25,6 +25,16 @@ class GroupsViewset(
 
         return qs
 
+    class StatsSerializer(serializers.Serializer):
+        groups_count = serializers.IntegerField()
+
+    @action(detail=False, methods=['GET'], url_path='stats')
+    def get_stats(self, request, *args, **kwargs):
+        groups_count = Group.objects.aggregate(groups_count=Count("*"))
+        
+        serializer = self.StatsSerializer(instance=groups_count)
+        return Response(serializer.data)
+
 class MembersViewset(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Member.objects.all()
 
@@ -41,6 +51,29 @@ class MembersViewset(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.Re
 
         return qs
 
+    class StatsSerializer(serializers.Serializer):
+        members_count = serializers.IntegerField()
+        members_by_groups = serializers.JSONField()
+        members_by_roles = serializers.JSONField()
+
+    @action(detail=False, methods=['GET'], url_path='stats')
+    def get_stats(self, request, *args, **kwargs):
+        members_count = Member.objects.aggregate(
+            count=Count("*")
+        )
+        members_by_groups = Member.objects.values('group__name').annotate(count=Count("group"))
+        members_by_roles = Member.objects.values('role').annotate(count=Count("role"))
+
+        stats = { 
+            'members_count': members_count['count'], 
+            'members_by_groups': members_by_groups, 
+            'members_by_roles': members_by_roles
+        }
+
+        serializer = self.StatsSerializer(instance=stats)
+
+        return Response(serializer.data)
+    
 class MemberImagesViewset(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = MemberImage.objects.all()
     serializer_class = MemberImageSerializer
@@ -63,19 +96,27 @@ class AlbumsViewset(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.Ret
 
     class StatsSerializer(serializers.Serializer):
         albums_count = serializers.IntegerField()
+        oldest_album_year = serializers.IntegerField()
+        newest_album_year = serializers.IntegerField()
         albums_by_groups = serializers.JSONField()
         albums_by_genres = serializers.JSONField()
         albums_by_years = serializers.JSONField()
 
     @action(detail=False, methods=['GET'], url_path='stats')
     def get_stats(self, request, *args, **kwargs):
-        albums_count = Album.objects.aggregate(count=Count("*"))
+        int_stats = Album.objects.aggregate(
+            count=Count("*"),
+            min_year=Min("year"),
+            max_year=Max("year")
+        )
         albums_by_groups = Album.objects.values('group__name').annotate(count=Count("group"))
         albums_by_genres = Album.objects.values('genre__name').annotate(count=Count("genre"))
         albums_by_years = Album.objects.values('year').annotate(count=Count("year"))
 
         stats = { 
-            'albums_count': albums_count['count'], 
+            'albums_count': int_stats['count'], 
+            'oldest_album_year': int_stats['min_year'],
+            'newest_album_year': int_stats['max_year'],
             'albums_by_groups': albums_by_groups, 
             'albums_by_genres': albums_by_genres, 
             'albums_by_years': albums_by_years
@@ -129,6 +170,16 @@ class GenresViewset(mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.Ret
             qs = qs.filter(user=self.request.user)
 
         return qs
+    
+    class StatsSerializer(serializers.Serializer):
+        genres_count = serializers.IntegerField()
+
+    @action(detail=False, methods=['GET'], url_path='stats')
+    def get_stats(self, request, *args, **kwargs):
+        genres_count = Genre.objects.aggregate(genres_count=Count("*"))
+        
+        serializer = self.StatsSerializer(instance=genres_count)
+        return Response(serializer.data)
 
 class UserProfileViewset(GenericViewSet):
     @action(url_path="info", detail=False, methods=["GET"])
