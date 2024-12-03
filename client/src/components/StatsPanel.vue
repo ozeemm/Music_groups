03563e2d29ab5045adcd153d4d889f5b0ac5b5c9
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-12-04 06:33:37 +0800

    Added StatsPanel component

---
+++ client/src/components/StatsPanel.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { useId, computed } from 'vue';
+
+    const unique_ud = useId()
+    const collapse_id = computed(() => { return `collapseStats_${unique_ud}` }) 
+
+</script>
+
+<template>
+     <div class="stats-panel">
+            <a class="text-reset text-decoration-none" :href="'#'+collapse_id" data-bs-toggle="collapse" role="button" aria-expanded="false" :aria-controls="collapse_id">
+                <h4 class="text-center">Статистика</h4>
+            </a>            
+            <div class="collapse show" :id="collapse_id">
+                <div class="d-flex row justify-content-center m-2">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+</template>
